4a58d4930972d9c940b2fa71194eab3c633a9391
Diky Efriansyah <dikyefriansyah2@gmail.com>
2022-07-28 18:55:05 +0700

    add task1 - FizzBuzz

---
+++ fizzbuzz-test.js
@@ -0,0 +1,21 @@
+let fizzBuzz = function() {
+
+    let arr = [];
+    
+    for(let i = 1;arr.length < 100;i++){
+    
+        if(i%15 == 0){
+            arr.push("FizzBuzz")
+        }else if(i%3 == 0){
+            arr.push("Fizz")
+        }else if(i%5 == 0){
+            arr.push("Buzz")
+        }else{
+            arr.push(i);
+        }
+    }
+    
+    return arr;
+    }
+    
+    console.log(fizzBuzz())
